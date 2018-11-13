d60447dbe855f890fcea56f8ce2e9ca205433fc8
github username <github email address>
2018-11-13 09:02:33 -0500

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
-def unsafe?(speed)
+require "pry"
 
+def unsafe?(speed)
+   if speed > 60 || speed < 40  
+        return true
+   else
+        return false
+        binding.pry
+   end
 end
 
 
-
 def not_safe?(speed)
-	
+   return speed > 60 || speed < 40? true : false
+   binding.pry
 end
 	
 
